7434c9424c3c9b91244de92723e7b1a8e1a44540
MrDecter <ghoillus@gmail.com>
2023-12-09 07:02:11 +0300

    Практика по ООП в классе Phone

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import oop_practice.Phone;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        // Практика по Phone
+        Phone phone1 = new Phone("89193555255", "Samsung", "30sm");
+        System.out.println("На ваш номер: " + phone1.getNumber() + " поступил звонок!");
+        phone1.recerev_call("Служба безопастности Сбербанка");
+        Phone.send_message("Вашу карту заблокировали, просьба сообщите CVV код для разблокировки!");
+         */
+    }
+}
