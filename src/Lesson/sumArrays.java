22f5a1f69697927e1201b1acc7a7394d5afd2d99
MrDecter <ghoillus@gmail.com>
2024-01-13 23:18:23 +0300

    Добавление урока по сумме массива

---
+++ src/Lesson/sumArrays.java
@@ -0,0 +1,17 @@
+package Lesson;
+
+import java.util.Arrays;
+
+public class sumArrays {
+    public static void main(String[] args){
+
+        int[] arrayX = {1321, 12, 54, -84, 22, -887, 74, 3, 5, 6, -9, 0, 1, 2, 3, 4}; // Исходный массив
+        int sum = 0;
+        for (int i = 0; i < arrayX.length; i++) {
+            sum = sum + arrayX[i];
+        }
+        System.out.println("Сумма массива = " + sum);
+    }
+
+
+}
